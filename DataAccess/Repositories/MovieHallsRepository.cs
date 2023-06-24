f19a03da7471ffe48bf38b4425ef97b1252ea8a7
Vitalik Knysh <knish.vital@gmail.com>
2023-06-24 23:55:25 +0300

    Removed the SaveChangesAsync from the movie halls repo

--- DataAccess/Repositories/MovieHallsRepository.cs
+++ DataAccess/Repositories/MovieHallsRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entities;
 using Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace DataAccess.Repositories;
 
@@ -19,18 +20,22 @@ public class MovieHallsRepository : IMovieHallsRepository
     public void Delete(MovieHall entity) =>
         _context.MovieHalls.Remove(entity);
 
-    public async Task<IList<MovieHall>> GetAllAsync() =>
-        await _context.MovieHalls
+    public async Task<IList<MovieHall>> GetAllAsync(int? pageNumber, int? pageSize)
+    {
+        var pageIndex = pageNumber.HasValue ? pageNumber.Value : 1;
+        var pageLimit = pageSize.HasValue ? pageSize.Value : 5;
+
+        return await _context.MovieHalls
             .Include(mh => mh.MovieSessions)
+            .Skip((pageIndex - 1) * pageLimit)
+            .Take(pageLimit)
             .ToListAsync();
+    }
 
     public async Task<MovieHall?> GetByIdAsync(Guid id) =>
         await _context.MovieHalls
             .Include(mh => mh.MovieSessions)
             .FirstOrDefaultAsync(mh => mh.Id == id);
-
-    public async Task SaveChangesAsync() =>
-        await _context.SaveChangesAsync();
 
     public void Update(MovieHall entity) =>
         _context.MovieHalls.Update(entity);
